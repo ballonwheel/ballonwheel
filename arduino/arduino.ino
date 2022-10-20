811254c89e9d90c6ce4cdebff937499e1d73930f
imre <imre@thinkpad.kati>
2022-10-20 20:56:03 +0200

    pwm with deadtime, in progress

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -75,7 +75,9 @@ uint8_t  sine_wave[256] = {
 
 /*
 ez a verzio:
-timer inditja ADCt
+timer1 pwm deadtime-al https://sites.google.com/site/greenmechatroniks/code-garage/pwm-with-dead-time
+
+timer2 inditja ADCt
 ADC inditja tx-et
 tx inditja rx-et
 
@@ -96,6 +98,21 @@ volatile uint8_t serialgo=0;
 volatile uint8_t adcDone;
 
 uint8_t k;
+
+
+/****** PWM deadtime ***********/
+// The bit resolution of the PWM
+#define PWM_resolution 8
+// Set the deadtime in microseconds
+const float T_DeadTime = 4.0f;
+ 
+// Conversion factor employed in PWM_mu (uint8_t)
+float A = 0.0f;
+// DeatTime converted to the bit range, i.e., 0-255 or 0-1023
+float deadTime_bits = 0.0f;
+/****************/
+
+
 
 //ISR_NOBLOCK
 ISR(TIMER0_COMPA_vect)
@@ -165,6 +182,56 @@ ISR(USART_TX_vect)
 }
 
 
+void PWM_mu(uint8_t mu)
+{
+	//--------------------------
+	// This function can be called anytime
+	// to change the duty cycle (mu) of the 
+	// PWM. The mu variable is in percents,
+	// i.e., from 0 to 100% hence the saturator.
+	//--------------------------
+
+	// Apply saturations
+	if (mu > 100)
+	{
+		mu = 100;
+	}
+
+	// set the PWM duty cycle for the first output
+	OCR1A = (float)(100 - mu) * A - deadTime_bits;
+
+	// set the PWM duty cycle for the second output
+	OCR1B = OCR1A + deadTime_bits;	    
+}
+
+
+void PWM_Init(void)
+{	
+	// Will hold the bit range
+	float PWM_bits = 0.0f;
+
+	// Set OC1A and OC1B as outputs
+	TCCR1A |= (1 << COM1A1)|(1 << COM1B1);
+	// Set OC1A as non inverted and OC1B as inverted
+	TCCR1A |= (0 << COM1A0)|(1 << COM1B0);
+    
+	// set the PWM mode
+	TCCR1A |= (0 << WGM10)|(0 << WGM11);
+	TCCR1B |= (0 << WGM12)|(1 << WGM13);
+    
+	//START the timer with no prescaler
+	TCCR1B |= (1 << CS10);
+
+	// Calculate the bit range from 0 to (2^bit - 1)
+	PWM_bits = pow(2, PWM_resolution) - 1.0f;
+	// Conversion made now to save calculations later
+	A = PWM_bits / 100.0f;
+	// Set TOP to PWM_bits (a range)
+	ICR1 = PWM_bits;
+	// Set the deadTime as a bit range (e.g. 0~255, 0~1023, etc.)
+	deadTime_bits = T_DeadTime * (F_CPU/1000000);
+}
+
 
 void setup() {
   /*************************/
@@ -213,14 +280,17 @@ void setup() {
 
 
 
-
+  /*
   pinMode(3, OUTPUT);
   pinMode(11, OUTPUT);
   TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20);
   TCCR2B = _BV(CS21) | _BV(CS20);;
   OCR2A = 50;
   OCR2B = 50;
-
+  */
+  //Set OC1A and 0C1B as PWM outputs
+  DDRB |= (1 << DDB1)|(1 << DDB2);
+  PWM_Init();
 
 
   //CLKPR = (1 << CLKPCE) + (0b111);	// System clk prescaler to 1/128
@@ -241,9 +311,9 @@ void setup() {
 /* LOOP */
 /**************************************************************************************************/
 void loop() {
-  OCR2A = sine_wave[k++];
-  OCR2B = sine_wave[k++];
-
+  //OCR2A = sine_wave[k++];
+  //OCR2B = sine_wave[k++];
+  PWM_mu(25);
 }
 
 
