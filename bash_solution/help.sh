733237cd5686d827dc439701a9b3392c1dee2a90
imre <dobany.hu@gmail.com>
2023-09-13 07:06:11 +0100

    readme is created for bash solution with main.c

--- bash_solution/help.sh
+++ bash_solution/help.sh
@@ -3,7 +3,13 @@
 echo "sudo ./run.sh [type] [uart]"
 echo "     [type] ch340, ftdi"
 echo "     [uart] 0, 1"
-echo "default"
-echo "sudo ./run.sh ch340 0"
+echo "no default config implemented yet"
+echo "do this expmle:"
+echo "sudo ./run.sh ftdi 0"
+echo "when you want to stop "
+echo "sudo ./stop.sh"
+echo "10msec kommunikacio elerheto, de bizonytalan az indulas, amig a keretek egymasra talalnak"
+echo "jelenleg 3 byte jon, megy; meg kene oldani 1, binaris kuldes, fogadast; elso bajt legyen a keretszam;vagy marad ASCII es akkor 4 byte [frame]['2' '5' '5']"
+
 
 
